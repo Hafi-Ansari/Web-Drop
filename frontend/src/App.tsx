cbdcff44cd6b190ad74b5d6d46e79cf869381254
hafi-ansari <hafi.m.ansari@gmail.com>
2023-08-28 11:40:41 -0500

    Basic UI created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,28 @@
-import 'tailwindcss/tailwind.css';
-function App() {
+import "tailwindcss/tailwind.css";
+import { MdOutlineWifiTethering } from "react-icons/md";
 
+function App() {
   return (
     <>
-    <h1 className="text-3xl font-bold underline">Web Drop</h1>
+      <div className="min-h-screen flex flex-col items-center justify-center space-y-4 bg-primaryBackground text-black">
+        <div className="mt-20 flex-1 flex flex-col items-center justify-center">
+          <h1 className="text-2xl text-lightGreen">
+            Open WebDrop on other devices to send files
+          </h1>
+          <h1 className="text-white">
+            Pair devices to be discoverable on other devices
+          </h1>
+        </div>
+        <div className="mt-auto flex flex-col items-center">
+          <h1 className="text-lightGreen text-7xl">
+            <MdOutlineWifiTethering />
+          </h1>
+          <h1 className="text-white">You are known as Anonymous Bastard</h1>
+          <h1 className="text-lightGreen mb-10">
+            You can be discovered by everyone on this network
+          </h1>
+        </div>
+      </div>
     </>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Add your color theme here
+        primaryBackground: "#001E2B",
+        dlp: "#012416",
+        darkGreen: "#023420",
+        midGreen: "#00684A",
+        lightGreen: "#00ED64",
+        midAccentGreen: "#00533b",
+      },
+    },
   },
   plugins: [],
 };
