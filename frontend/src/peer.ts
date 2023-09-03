397abb585755c97a263b57f8c819e3f84b3a81b1
hafi-ansari <hafi.m.ansari@gmail.com>
2023-09-03 18:03:58 -0500

    Ready for deployment

--- frontend/src/peer.ts
+++ frontend/src/peer.ts
@@ -2,7 +2,7 @@ import { Peer } from "peerjs";
 import { v4 as uuidv4 } from "uuid";
 
 const peer = new Peer(uuidv4(), {
-  host: '192.168.1.97',  // or import from environment variable
+  host: import.meta.env.VITE_IP,  
   port: 3000,
   path: '/peerjs',
 });
